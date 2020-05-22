4dec99815dbb4453dc2cd0cbfd37be631838a3e1
SUGAI-AYAKA <1995.0828.ayaka@gmail.com>
2020-05-22 02:41:13 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
-  
+   root to: 'toppages#index'
+   
+   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] do 
+  resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followings
       get :followers
       get :likes
-      
     end
   end
-  resources :microposts, only: [:create, :destroy] do
-    member do
-      get :micropostlikeusers
-    end
-  end
-   
-  resources :relationships, only: [:create, :destroy]
-  resources :favorites, only: [:create, :destroy]
-end
+
+  resources :microposts, only: [:create, :destroy]  
+  resources :relationships, only: [:create, :destroy] 
+  resources :favorites, only: [:create, :destroy] 
+  end 
+
